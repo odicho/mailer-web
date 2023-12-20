89ca3780032271ae9b4928801b7164a1860a03de
Amanuel Odicho <amanuelodicho@gmail.com>
2023-12-19 20:36:25 -0600

    formatted some files

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,5 @@
-import styles from "./page.module.css";
-
+import styles from './page.module.css';
 
 export default function Page(): JSX.Element {
-  return (
-    <main className={styles.main}>
-      hello world
-    </main>
-  );
+	return <main className={styles.main}>hello world</main>;
 }

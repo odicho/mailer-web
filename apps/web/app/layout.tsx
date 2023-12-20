8d1c34aec38d745a826a7adf8f24a5c180484cee
Amanuel Odicho <amanuelodicho@gmail.com>
2023-12-20 00:16:24 -0600

    add auth package using lucia

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { db } from '@repo/db';
 
 const inter = Inter({ subsets: ['latin'] });
 
---
+++ packages/auth/src/lucia.ts
@@ -0,0 +1,59 @@
+import { db } from '@repo/db';
+import { session, user } from '@repo/db/schema';
+import { Lucia } from 'lucia';
+import { DrizzleMySQLAdapter } from '@lucia-auth/adapter-drizzle';
+import { GitHub } from 'arctic';
+import { cookies } from 'next/headers';
+import { cache } from 'react';
+
+const drizzleAdapter = new DrizzleMySQLAdapter(db, session, user);
+
+export const lucia = new Lucia(drizzleAdapter, {
+	getSessionAttributes: (attributes) => {
+		return {};
+	},
+	getUserAttributes: (attributes) => {
+		return {
+			username: attributes.username,
+		};
+	},
+	sessionCookie: {
+		attributes: {
+			secure: process.env.NODE_ENV === 'production',
+		},
+	},
+});
+
+declare module 'lucia' {
+	interface Register {
+		Lucia: typeof lucia;
+	}
+	// interface DatabaseSessionAttributes {}
+	interface DatabaseUserAttributes {
+		username: string;
+	}
+}
+
+export const githubAuth = new GitHub(
+	process.env.GITHUB_CLIENT_ID ?? '',
+	process.env.GITHUB_CLIENT_SECRET ?? '',
+);
+
+export const getUser = cache(async () => {
+	const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+	if (!sessionId) return null;
+	const { session, user } = await lucia.validateSession(sessionId);
+	try {
+		if (session?.fresh) {
+			const sessionCookie = lucia.createSessionCookie(session.id);
+			cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+		}
+		if (!session) {
+			const sessionCookie = lucia.createBlankSessionCookie();
+			cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+		}
+	} catch {
+		// Next.js throws error when attempting to set cookies when rendering page
+	}
+	return user;
+});
